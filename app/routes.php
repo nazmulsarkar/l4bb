3e976091b36909eaac31563db5907fa0bbfe4adb
Nazmul <nazmul.phpdroidx@gmail.com>
2014-02-12 23:40:41 +0600

    added basic stuffs and renamed the application layout file

--- app/routes.php
+++ app/routes.php
@@ -10,8 +10,11 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-
+ 
+//backbone app route
 Route::get('/', function()
 {
-	return View::make('hello');
+  //change our view name to the view we created in a previous step
+  //notice that we do not need to provide the .mustache extension
+  return View::make('layouts.application')->nest('content', 'app');
 });
